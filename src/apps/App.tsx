def8e0b434805d97d76d846e130d9c0833736ba4
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-09 17:26:20 +0300

    Init Commit 🎉

---
+++ src/apps/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../assets/scss/app.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1 className='display-3'>Zaheen App</h1>
+    </div>
+  );
+}
+
+export default App;
