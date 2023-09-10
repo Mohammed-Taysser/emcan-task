63a098820c02b867dcdeceaaa00bc1a7440dafdd
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-10 13:56:24 +0300

    add app section

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,10 +1,33 @@
+import About from '../components/About';
+import Instructors from '../components/Instructors';
+import PerfectCourses from '../components/PerfectCourses';
+import Testimonials from '../components/Testimonials';
+import TopSubjects from '../components/TopSubjects';
+import Footer from '../layouts/Footer';
+import Navbar from '../layouts/Navbar';
+import Slider from '../layouts/Slider';
+
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../assets/scss/app.scss';
 
 function App() {
   return (
     <div className='app'>
-      <h1 className='display-3'>Zaheen App</h1>
+      <Navbar />
+
+      <Slider />
+
+      <TopSubjects />
+
+      <PerfectCourses />
+
+      <Instructors />
+
+      <About />
+
+      <Testimonials />
+
+      <Footer />
     </div>
   );
 }
