eac45a658aeff330518a088374cc52829c7c95c1
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-10 13:56:15 +0300

    add app typescript types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,44 @@
 /// <reference types="vite/client" />
+
+interface Subject {
+  id: number;
+  label: string;
+  url: string;
+  image: string;
+}
+
+interface Course {
+  id: number;
+  title: string;
+  instructor: {
+    avatar: string;
+    name: string;
+  };
+  amount: number;
+  rating: number;
+  price: number;
+  image: string;
+}
+
+interface Testimonial {
+  id: number;
+  name: string;
+  body: string;
+  date: string;
+  star: number;
+}
+
+interface Slide {
+  id: number;
+  title: string;
+  image: string;
+  subtitle: string;
+}
+
+interface Instructor {
+  id: number;
+  name: string;
+  job: string;
+  about: string;
+  avatar: string;
+}
